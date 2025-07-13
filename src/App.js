6b4ef035743528e4aeb92a2035a9e08c430c9ffc
ismael2314 <ismaelyusuf1221@gmail.com>
2025-07-13 20:11:21 +0300

    new file:   .gitignore 	modified:   README.md 	modified:   public/data2.json 	modified:   src/App.js 	modified:   src/components/Container.jsx 	modified:   src/pages/Home.jsx 	modified:   src/pages/Project.jsx 	modified:   src/pages/index.css 	new file:   src/template/template.jsx

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Home from "./pages/Home";
-import Project from "./pages/Project";
-import Nav from "./components/Nav";
 import ProImg from './assets/img/profile.jpg'
-import { Social } from "./components/Container"
-import Contact from "./pages/Contact";
+import Template from "./template/template";
+import { useEffect, useState } from "react";
 function App() {
+  const [path,setPath]= useState([]);
+  const [data,setData]=useState({})
+  useEffect(()=>{
+          const url = window.location.origin
+          fetch(`${url}/data2.json`)
+          .then(data=>{return data.json();})
+          .then(json=>{
+              setPath(Object.keys(json))
+              setData(json)
+            })
+          .catch(err=>console.error("Error while fetching data from ",url))
+          .finally(console.log("Request done!"))
+      },[]);
   return (
-    <div className="container">
+    <div className="container-fluid">
       <BrowserRouter>
-        <Nav header="Ismael" logo={ProImg}/>
+        {/* <Nav header="Ismael" logo={ProImg}/> */}
         <Routes>
-          <Route path="/" exact element={<Home src={ProImg}/>} />
-          <Route path="/projects" exact element={<Project src={ProImg}/>} />
-          <Route path="/p/:id" exact element={<Project />} />
-          <Route path="/contact" exact element={<Contact />} />
-          <Route path="/cv" exact element={{}} />
+          <Route path="/" element={<Template/>}>
+            {
+              path.map((e)=>{
+                  return <Route path={`/${e}`} exact element={<Home src={ProImg} data={data[`${e}`]} selected={e} nav={path} action={()=>{}}/>}/>
+              })
+            }
+          </Route>
         </Routes>
-        <Social/>
+        
       </BrowserRouter>
     </div>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,23 @@
-import { Hero, Skills } from "../components/Container"
+import { useEffect, useState } from "react"
+import { Hero } from "../components/Container"
 
 const Home = (prop)=>{
+    const [profile,setProfile]=useState({})
+
+    useEffect(()=>{
+        setProfile(prop?.data)
+    },[]);
+    
     return(
         <div>
-            <Hero src={prop.src}/>
-            <Skills/>
+            <Hero 
+                src={prop.src} 
+                list={profile.detail} 
+                detail={profile.summary} 
+                data={profile.data}
+                nav={prop.nav} 
+                navSelected={prop.selected}
+                action={prop.setCurrentNav}/>
         </div>
     )
 }
